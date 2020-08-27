4302e9ea909f42dacd4a1626e0388d4a6d492ee9
WindSpirit <simon343riley@gmail.com>
2020-08-27 14:58:10 +0800

    Format

--- backend/model/value.go
+++ backend/model/value.go
@@ -1,11 +1,11 @@
 package model
 
 const (
-	DB_FILE_NAME         	= "nav_page.db"
-	DB_TABLE_USER        	= "user"
-	DB_USER_DEFAULT_ID	 	= 1001
-	DB_USER_DEFAULT_USER 	= "admin"
-	DB_USER_DEFAULT_PASSWD 	= "passwd"
+	DB_FILE_NAME           = "nav_page.db"
+	DB_TABLE_USER          = "user"
+	DB_USER_DEFAULT_ID     = 1001
+	DB_USER_DEFAULT_USER   = "admin"
+	DB_USER_DEFAULT_PASSWD = "passwd"
 
-	ERROR_PASSWD_WRONG		= "crypto/bcrypt: hashedPassword is not the hash of the given password"
+	ERROR_PASSWD_WRONG = "crypto/bcrypt: hashedPassword is not the hash of the given password"
 )
