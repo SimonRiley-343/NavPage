2dba56d463117c72ab37402c913ce4d4d38c7c24
WindSpirit <simon343riley@gmail.com>
2021-03-18 15:50:02 +0800

    Update page Login

--- view/src/components/Login.vue
+++ view/src/components/Login.vue
@@ -1,8 +1,14 @@
 <template>
     <el-container>
-        <el-main>
+        <el-main class="login-main">
             <el-row>
-                <el-col :span="8" :offset="8"></el-col>
+                <el-col :span="8" :offset="8" class="login-col-title">NavPage Web Admin</el-col>
+                <el-col :span="8" :offset="8" class="login-col-passwd">
+                    <el-input placeholder="Password" v-model="password" show-password></el-input>
+                </el-col>
+                <el-col class="login-col-btn">
+                    <el-button type="primary" round>Login</el-button>
+                </el-col>
             </el-row>
         </el-main>
     </el-container>
@@ -15,16 +21,27 @@ import { login } from '@/utils/api';
 export default Vue.extend({
     name: 'Login',
     data() {
-        return {};
+        return {
+            password: ''
+        };
     },
     mounted() {
-        // this.login();
+        if (localStorage.getItem('sessionId')) {
+            this.login();
+        }
     },
     methods: {
         login() {
-            login()
+            const reqData = {
+                passwd: this.password
+            };
+            login(reqData)
                 .then((res) => {
-                    // TODO: Check login status
+                    if (res.status) {
+                        this.$router.push({
+                            name: 'Admin'
+                        });
+                    }
                 })
                 .catch((e) => {
                     console.log(e);
@@ -35,4 +52,22 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
+.login-main {
+    margin-top: 100px;
+}
+
+.login-col-title {
+    text-align: center;
+    font-size: 34px;
+    font-weight: 600;
+}
+
+.login-col-passwd {
+    margin-top: 80px;
+}
+
+.login-col-btn {
+    margin-top: 40px;
+    text-align: center;
+}
 </style>
